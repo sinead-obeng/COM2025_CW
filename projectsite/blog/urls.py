9a9ec179a99a63a14ee15f89d77b2767843836eb
sinead-obeng <sinead.obeng@gmail.com>
2022-10-14 12:50:41 +0100

    Creating the contact page
    Added templates and urls to start the contact page. There is a base template that the contact page inherits from.

--- projectsite/blog/urls.py
+++ projectsite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('contact/', views.contact, name='blog-contact'),
 ]
--- projectsite/blog/views.py
+++ projectsite/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
     return render(request, "blog/home.html")
+
+
+def contact(request):
+    return render(request, "blog/contact.html")
