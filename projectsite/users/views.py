f6e68a778b3925d86513ae9a9bce3629a33609b9
sinead-obeng <sinead.obeng@gmail.com>
2022-10-16 00:27:41 +0100

    Completing the profile page
    Completed the profile page by adding forms, models and creating a view.

--- projectsite/users/views.py
+++ projectsite/users/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 # Create your views here.
@@ -25,4 +25,24 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            messages.success(
+                request, f"Your account has been updated!"
+            )
+            return redirect("profile")
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        "user_update_form": user_update_form,
+        "profile_update_form": profile_update_form
+    }
+    return render(request, "users/profile.html", context)
