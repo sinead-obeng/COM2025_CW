70d9c116b1dcef579913371d5f3c0ed0fe666301
sinead-obeng <sinead.obeng@gmail.com>
2022-11-29 18:52:45 +0000

    Creating model unit tests for users app

---
+++ projectsite/users/tests/test_models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from users.models import Profile
+
+
+# Create your tests here.
+class UserModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.all().delete()
+        u1 = User.objects.create_user(username="testuser1", password="12345")
+        u1.save()
+        u2 = User.objects.create_user(username="testuser2", password="12345")
+        u2.save()
+        u3 = User.objects.create_user(username="testuser3", password="12345")
+        u3.save()
+
+    def test_setup_data(self):
+        self.assertEquals(3, User.objects.all().count())
+        self.assertEquals(3, Profile.objects.all().count())
